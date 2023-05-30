9d3d1defab00eedb9e3aaf37e76239ce4f849d51
vitalikx100 <91387974+vitalikx100@users.noreply.github.com>
2023-05-31 00:07:01 +0400

    Добавьте файлы проекта.

---
+++ Assembler_lab7.1/Assembler_lab7.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // 2^((x + lnx)* log2(5))
+    double result, x;
+    double c5 = 5.0;
+    cout << "Input x: ";
+    cin >> x;
+    _asm
+    {
+        finit;
+        fldln2; st(0) = ln(2)
+        fld x; st(0) = x; st(1) = ln(2)
+        fyl2x; st(0) = ln(x)
+        fld x; st(0) = x; st(1) = ln(x)
+        fadd st, st(1); st(0) = x + lnx
+        fld1; st(0) = 1
+        fld c5; st(0) = 5; st(1) = 1; st(2) = x + lnx
+        fyl2x; st(0) = log2(5); st(1) = x + lnx
+        fmulp st(1), st; st(0) = (x + lnx) * log2(5)        
+
+        fstp result;
+      
+
+    }
+     
+    cout << "asm: " << pow(2,result) << endl;
+    cout << "C++: " << pow(5, x + log(x));
+}
